9c522e061cffb89c21904d28333fdb0554c19a37
grecheskiy <grecheskiy@rambler.ru>
2023-02-12 23:12:41 +0300

    add homework_9_3

---
+++ homework_9_3/Program.cs
@@ -0,0 +1,9 @@
+﻿//Показать натуральные числа от M до N, N и M заданы
+Console.Write("Enter min number M: ");
+int M = int.Parse(Console.ReadLine() ?? "");
+Console.Write("Enter max number N: ");
+int N = int.Parse(Console.ReadLine() ?? "");
+string Numbers(int M, int N)
+{if (M <= N) return $"{M} " + Numbers(M+1,N); 
+else return string.Empty;}
+Console.WriteLine(Numbers(M,N));
