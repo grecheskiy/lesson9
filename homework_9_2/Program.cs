964368d40231e63f6ceb1c8ae016d7ecae9225c9
grecheskiy <grecheskiy@rambler.ru>
2023-02-12 22:58:02 +0300

    add homework_9_2

---
+++ homework_9_2/Program.cs
@@ -0,0 +1,8 @@
+﻿//Показать натуральные числа от N до 1, N задано
+Console.Write("Enter number N: ");
+int N = int.Parse(Console.ReadLine() ?? "");
+int a = 1;
+string Numbers(int a, int N)
+{if (a <= N) return Numbers(a+1,N) + $"{a} "; 
+else return string.Empty;}
+Console.WriteLine(Numbers(a,N));
