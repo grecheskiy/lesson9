ff46e43ead91f134444bb4e201a58184552a1d8a
grecheskiy <grecheskiy@rambler.ru>
2023-02-12 23:26:45 +0300

    add homework_9_4

---
+++ homework_9_4/Program.cs
@@ -0,0 +1,9 @@
+﻿//Найти сумму элементов от M до N, N и M заданы
+Console.Write("Enter min number M: ");
+int M = int.Parse(Console.ReadLine() ?? "");
+Console.Write("Enter max number N: ");
+int N = int.Parse(Console.ReadLine() ?? "");
+int Numbers(int M, int N)
+{if (N == 0) return 0;
+else return N + Numbers(M,N-1); }
+Console.WriteLine(Numbers(M,N));
