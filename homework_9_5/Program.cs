028723a2f32f5c728bc6ff21bf1c9ffc0f306f70
grecheskiy <grecheskiy@rambler.ru>
2023-02-13 23:02:52 +0300

    add homework_9_5

---
+++ homework_9_5/Program.cs
@@ -0,0 +1,8 @@
+﻿//Найти сумму цифр числа
+Console.Write("Enter number 1 to 99: ");
+int number = int.Parse(Console.ReadLine() ?? "");
+int SumNumber(int number)
+{if (number < 0 || number >= 100 )
+return 0;
+else return (number / 10) + (number % 10);
+}Console.WriteLine(SumNumber(number));
