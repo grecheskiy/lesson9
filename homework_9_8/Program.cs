d78387c881ddcf903ddb5cfb1580b64a79689731
grecheskiy <grecheskiy@rambler.ru>
2023-02-14 01:21:29 +0300

    add homework_9_8

---
+++ homework_9_8/Program.cs
@@ -0,0 +1,19 @@
+﻿//Написать программу показывающие первые N чисел, 
+//для которых каждое следующее равно сумме двух предыдущих. 
+//Первые два элемента последовательности задаются пользователем
+Console.Write("Enter x: ");
+int x = int.Parse(Console.ReadLine() ?? "");
+Console.Write("Enter y: ");
+int y = int.Parse(Console.ReadLine() ?? "");
+int N = 10;
+int Metod(int a, int b, int n)
+{
+if (n == 1) return a;
+if (n == 2) return b;
+return Metod(a, b, n - 1) + Metod(a, b, n - 2);
+}
+for (int i = 1; i < N; i++)
+{
+Console.Write(Metod(x, y, i) + " ");
+}
+Console.WriteLine();
